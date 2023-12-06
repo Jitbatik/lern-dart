122ae904f3efb9994617e5af8a232f0ebfbcb126
Jitbatik <kvazzsik@gmail.com>
2023-12-06 23:02:32 +0300

    Init project

---
+++ bin/lern_df.dart
@@ -0,0 +1,5 @@
+import 'package:lern_df/lern_df.dart' as lern_df;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lern_df.calculate()}!');
+}
---
+++ test/lern_df_test.dart
@@ -0,0 +1,8 @@
+import 'package:lern_df/lern_df.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
